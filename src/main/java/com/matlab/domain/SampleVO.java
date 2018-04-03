a330cef6de333c35b35b517e5ba588b9ef24a00f
chopinfrog <skori@knou.ac.kr>
2018-04-03 12:04:05 +0900

    sample

---
+++ src/main/java/com/matlab/domain/SampleVO.java
@@ -0,0 +1,14 @@
+package com.matlab.domain;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString(exclude= {"val3"})
+public class SampleVO {
+
+	private String val1;
+	private String val2;
+	private String val3;
+		
+}
